4b85f4cb38223cc65fbf35171c8743e3e7b3c04d
vladimirovodenko <vladimir@tallinn-learning.ee>
2026-03-19 18:58:42 +0200

    Initial commit

---
+++ tests/auth.spec.ts
@@ -0,0 +1,57 @@
+import { expect, test } from '@playwright/test'
+import { Login } from '../dto/login-dto'
+
+let loginDto: Login
+const baseUrl = 'https://backend.tallinn-learning.ee'
+const loginEndpoint = '/login/student'
+const ordersEndpoint = '/orders'
+
+test.describe.serial('Authorization flow', () => {
+  test.beforeAll(() => {
+    loginDto = new Login(
+      process.env['DL_USERNAME']!,
+      process.env['DL_PASSWORD']!,
+    )
+  })
+
+  test('should login and receive authorization token', async ({ request }) => {
+    const response = await request.post(baseUrl + loginEndpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+      data: loginDto,
+    })
+
+    expect(response.status()).toBe(200)
+    const token = await response.text()
+    console.log('Received token:', token)
+    expect(token).toBeTruthy()
+  })
+
+  test('should get orders with authorization token', async ({ request }) => {
+    const loginResponse = await request.post(baseUrl + loginEndpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+      data: loginDto,
+    })
+
+    expect(loginResponse.status()).toBe(200)
+    const token = await loginResponse.text()
+    expect(token).toBeTruthy()
+
+    const response = await request.get(baseUrl + ordersEndpoint, {
+      headers: {
+        accept: '*/*',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    expect(response.status()).toBe(200)
+    const orders = await response.json()
+    console.log('Orders:', JSON.stringify(orders, null, 2))
+    expect(orders).toBeTruthy()
+  })
+})
